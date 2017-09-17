c55a29938ae787a9e16ca0b7331dfe8a32014761
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2017-09-16 22:03:41 -0700

    Find users on github

--- js.js
+++ js.js
@@ -32,8 +32,45 @@ $(document).ready(function(){
 		var lastName = $("#lastname").val();
 		let fullName = {name, lastName};
 		people.push(fullName);
+		$("#name").val("");
+		$("#lastname").val("");
 		showUsers();
+	});
+
+	// $.ajax({
+	// 	method:"GET",
+	// 	url:"https://jsonplaceholder.typicode.com/posts",
+	// 	dataType:"json"
+	// }).done(function(data){
+	// 	data.map(function(a,i){
+	// 		$("#description").append(`<p>${a.title}</p>`)
+	// 	})
+	// });
+
+	//Find Github User
+	$("#githubFinder").on('submit', function(e){
+		e.preventDefault();
+		var name = $("#githubName").val();
+		$.ajax({
+			method:"GET",
+			url: "https://api.github.com/users/" + name
+		}).done(function(data){
+			var image = data.avatar_url;
+			var userName = data.login;
+			var followers = data.followers;
+			$("#githubInfo").html(
+					"<img width='100px' src="+image+"> " +
+					"<h3>"+userName+"</h3>" +
+					"<h3>"+followers+"</h3>"
+				);
+
+				$("#githubName").val("");
+		});
 	});
 
 
 });
+
+// <img src="https://placehold.it/75" alt="">
+// <h3>Username</h3>
+// <h3>23.24</h3>
